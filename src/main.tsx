89624a8e0cbf5afb6da1c650c91726d802c066d0
jashton-stack <jashton@ashtonaisolutions.com>
2025-12-29 14:31:39 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
+// src/main.tsx
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 
-const root = document.getElementById("root")!;
-createRoot(root).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
